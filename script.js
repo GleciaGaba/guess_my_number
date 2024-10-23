0194212428dd12ed3edf4f773f458f6542c40d3c
GleciaGaba <gleciasf@hotmail.com>
2024-10-23 10:51:40 +0200

    update

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highscore = Number(localStorage.getItem('highscore')) || 0;
 
 //Nous allons maintenant créer la logique du HighScores. Créez
 //tout d’abord sous la variable score, une variable « highScore »
@@ -80,6 +81,7 @@ check.addEventListener('click', event => {
 
     if (score > highScore) {
       document.querySelector('.highscore').textContent = score;
+      highscoreContent.textContent = highScore;
     }
   } else if (guess !== secretNumber) {
     displayMessage(guess > secretNumber ? 'Too high !' : 'Too low !');
@@ -128,3 +130,61 @@ document.querySelector('.again').addEventListener('click', function () {
 //Il faudra pour cela créer un if dans le
 //bloc où le jour a gagné. La condition est simple, si le score du
 //joueur est supérieur au highscore alors …
+
+//('use strict');
+//
+//let secretNumber = Math.trunc(Math.random() * 20 + 1);
+//
+//const check = document.querySelector('.check');
+//const again = document.querySelector('.again');
+//const highscoreContent = document.querySelector('.highscore');
+//
+//let score = 20;
+//let highScore = Number(localStorage.getItem('highscore')) || 0;
+//highscoreContent.textContent = highScore;
+//
+//function displayMessage(message) {
+//  document.querySelector('.message').textContent = message;
+//}
+//
+//check.addEventListener('click', () => {
+//  const guess = Number(document.querySelector('.guess').value);
+//
+//  if (!guess) {
+//    displayMessage('❌ No Number');
+//  } else if (guess !== secretNumber) {
+//    displayMessage(`${guess > secretNumber ? '📈 Too high' : '📉 Too low'}`);
+//    score--;
+//    document.querySelector('.score').textContent = score;
+//    if (score < 1) {
+//      score = 0;
+//      document.querySelector('.score').textContent = score;
+//      displayMessage('You lost the game');
+//      check.disabled = true;
+//      document.querySelector('.guess').disabled = true;
+//    }
+//  } else {
+//    displayMessage('🏆 Correct Number');
+//    document.body.style.backgroundColor = ' #60b347';
+//    document.querySelector('.number').style.width = '30rem';
+//    if (score > highScore) {
+//      highScore = score;
+//      localStorage.setItem('highscore', highScore);
+//      highscoreContent.textContent = highScore;
+//    }
+//  }
+//});
+//
+//again.addEventListener('click', () => {
+//  score = 20;
+//  secretNumber = Math.trunc(Math.random() * 20 + 1);
+//  displayMessage('Start guessing...');
+//  document.querySelector('.score').textContent = score;
+//  document.querySelector('.guess').value = '';
+//  document.body.style.backgroundColor = '#222';
+//  document.querySelector('.number').style.width = '15rem';
+//  document.querySelector('.number').textContent = '?';
+//  check.disabled = false;
+//  document.querySelector('.guess').disabled = false;
+//});
+//
